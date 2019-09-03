0f9fc4442650720fa881000a3a12f0462632fc0d
Peter ZAŤKO <Peter_ZATKO@tempest.sk>
2019-09-03 14:56:22 +0200

    [SNKKB-60] Continuous Integration Bamboo setup

--- src/environments/environment.tempest-dev.ts
+++ src/environments/environment.tempest-dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:8080',
-  snkokBackOfficeUrl: 'http://localhost:8080',
+  url: 'http://10.201.37.153:8080',
+  snkokBackOfficeUrl: 'http://10.201.37.111:8080',
   siteKey: '6Ld7oqUUAAAAAORInrRX9rvdZJgwEtg5cAhvOVnD',
   passwordPattern: '(?=.*\\d)(?=.*[a-zA-Z]).{6,}'
 };
