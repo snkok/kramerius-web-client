365ba62afe832c651d145b2468079099e696ed5b
Peter ZAŤKO <Peter_ZATKO@tempest.sk>
2019-09-13 15:17:08 +0200

    [SNKKB-76] Vypnut ako default posielanie statistik do google vo webclientovi

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 
+declare var APP_GLOBAL: any;
+
 @Injectable()
 export class AnalyticsService {
 
   sendEvent(category: string, action: string, label: string = '') {
-    (<any>window).gaaa('send', 'event', category, action, label);
+    if (APP_GLOBAL.ga && 0 < APP_GLOBAL.ga.length) {
+      (<any>window).gaaa('send', 'event', category, action, label);
+    }
   }
 
 }
