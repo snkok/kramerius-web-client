ae747047e15385b6569425b8dbe99e4018ba9ea9
Peter ZAŤKO <Peter_ZATKO@tempest.sk>
2019-09-27 16:40:11 +0200

    [SNKKB-88] Treba nahradit ngx-youtube-player vo web clientovi

--- src/app/home/owl-carousel/owl-carousel.component.ts
+++ src/app/home/owl-carousel/owl-carousel.component.ts
@@ -1,11 +1,11 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {OwlCarousel} from 'ngx-owl-carousel';
 
 @Component({
   selector: 'app-owl-carousel',
   templateUrl: './owl-carousel.component.html',
 })
-export class OwlCarouselComponent {
+export class OwlCarouselComponent implements OnInit {
   @ViewChild('owlElement') owlElement: OwlCarousel;
 
 
@@ -26,16 +26,6 @@ export class OwlCarouselComponent {
     onTranslate: this.onTranslateCallback.bind(this),
   };
 
-  video1Id = 'ee6whfngY4E';
-  video2Id = 'o0k5WOsRd4Y';
-  videoWidth = '100%';
-  videoHeight = '100%';
-
-  private yt1Event;
-  private yt2Event;
-  private player1;
-  private player2;
-
   images = [
     {
       image: '../../../assets/img/snk_carousel_img1.jpg'
@@ -45,28 +35,85 @@ export class OwlCarouselComponent {
     },
   ];
 
-  onVideo1StateChange(event) {
-    this.yt1Event = event.data;
-    this.owlElement.trigger('stop.owl.autoplay');
-  }
+   video1Id = 'ee6whfngY4E';
+   video2Id = 'o0k5WOsRd4Y';
 
-  onVideo2StateChange(event) {
-    this.yt2Event = event.data;
-    this.owlElement.trigger('stop.owl.autoplay');
-  }
+   private yt1Event;
+   private yt2Event;
+   private player1;
+   private player2;
 
-  savePlayer1(player) {
-    this.player1 = player;
-  }
-  savePlayer2(player) {
-    this.player2 = player;
-  }
+   onVideo1StateChange(event) {
+     this.yt1Event = event.data;
+     this.owlElement.trigger('stop.owl.autoplay');
+   }
 
-  onTranslateCallback() {
-    if (this.yt1Event === 1) {
-      this.player1.pauseVideo();
-    } else if (this.yt2Event === 1) {
-      this.player2.pauseVideo();
+   onVideo2StateChange(event) {
+     this.yt2Event = event.data;
+     this.owlElement.trigger('stop.owl.autoplay');
+   }
+
+   onTranslateCallback() {
+     if (this.yt1Event === 1) {
+       this.player1.pauseVideo();
+     } else if (this.yt2Event === 1) {
+       this.player2.pauseVideo();
+     }
+   }
+
+  init() {
+    // Return if Player is already created
+    if (window['YT']) {
+      this.initPlayers();
+      return;
     }
+
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    /* 3. startVideo() will create an <iframe> (and YouTube player) after the API code downloads. */
+    window['onYouTubeIframeAPIReady'] = () => this.initPlayers();
+  }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  initPlayers() {
+    this.player1 = new window['YT'].Player('player1', {
+      videoId: this.video1Id,
+      playerVars: {
+        autoplay: 0,
+        modestbranding: 1,
+        controls: 1,
+        disablekb: 1,
+        rel: 0,
+        showinfo: 0,
+        fs: 0,
+        playsinline: 1
+      },
+      events: {
+        'onStateChange': this.onVideo1StateChange.bind(this),
+      }
+    });
+
+    this.player2 = new window['YT'].Player('player2', {
+      videoId: this.video2Id,
+      playerVars: {
+        autoplay: 0,
+        modestbranding: 1,
+        controls: 1,
+        disablekb: 1,
+        rel: 0,
+        showinfo: 0,
+        fs: 0,
+        playsinline: 1
+      },
+      events: {
+        'onStateChange': this.onVideo2StateChange.bind(this),
+      }
+    });
   }
 }
